b590ed4878ac46c4ec930492c511ef6d935b85cc
Qedrigord <tasaras200@gmail.com>
2025-02-15 18:36:57 +0200

    added user page

--- routes/login_route.py
+++ routes/login_route.py
@@ -1,6 +1,10 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 
 def init_login_route(app):
     @app.route('/')
     def login():
         return render_template('login.html')
+
+    @app.route('/redirect_page', methods=['POST'])
+    def redirect_page():
+        return render_template('redirect_page.html')
