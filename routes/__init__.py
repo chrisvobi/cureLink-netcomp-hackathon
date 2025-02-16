4ae3946682f500c294352e4f253f3c3c8076111e
Qedrigord <tasaras200@gmail.com>
2025-02-16 20:11:24 +0200

    added acount details and appointment history

--- routes/__init__.py
+++ routes/__init__.py
@@ -4,6 +4,7 @@ from .agent_route import init_agent_route
 from .appointments_route import init_appointments_route
 from .register_route import init_register_route
 from .appointments_history_route import init_appointments_history_route
+from .account_route import init_account_route
 
 def init_routes(app):
     init_login_route(app)
@@ -12,4 +13,5 @@ def init_routes(app):
     init_appointments_route(app)
     init_register_route(app)
     init_appointments_history_route(app)
+    init_account_route(app)
     
